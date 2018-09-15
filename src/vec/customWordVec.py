e424ee86ce8c08338cf3a366c9c0ec42807f2d87
vitrioil <41074533+vitrioil@users.noreply.github.com>
2018-09-15 19:28:25 +0530

    Add files via upload

--- src/vec/customWordVec.py
+++ src/vec/customWordVec.py
@@ -4,10 +4,14 @@ import nltk
 import keras
 import gensim
 import pickle
+import psutil
 import logging
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+
+def memory_above(critical_val = 95):
+	return (psutil.virtual_memory().percent > critical_val)
 
 def imdb_dataset():
 	NUM_WORDS=1000 # only use top 1000 words
@@ -63,19 +67,19 @@ def preprocess(location="../input/train.csv", itr=None, tokenized=False):
 			sent = nltk.word_tokenize(sent)
 		for word in sent:
 			if word_set.get(word) is None:
-				word_indx = len(word_set) 
+				word_indx = len(word_set) + 1
 				word_set[word] = word_indx
 
 	def generate_pairs(word_distance, batch_size, itr=None):
-		''' 
-		itr should be in tokenized form
-		'''
 		if itr is None:
 			itr = df["text"]
 		context,target = [],[]
 		total_sentences = len(itr) 
-		for sent_indx,sentence in enumerate(shuffle(itr)):
-			for indx,words in enumerate(sentence):
+		for sent_indx, sentence in enumerate(shuffle(itr)):
+			if not tokenized:
+				tokenized_list = nltk.word_tokenize(sentence)
+			for indx, context_word in enumerate(tokenized_list):
+				'''
 				for distance in range(indx - word_distance,indx + word_distance):
 					if distance == indx or distance < 0 or distance >= len(sentence):
 						continue
@@ -84,10 +88,23 @@ def preprocess(location="../input/train.csv", itr=None, tokenized=False):
 					if len(context) == batch_size or sent_indx==total_sentences-1:
 						yield np.array(context),np.array(target)[...,np.newaxis]
 						context,target = [],[]
+				'''
+				#Sampling is suggested
+				context_row = [] 
+				for distance in range(indx - word_distance, indx + word_distance):
+					if distance == indx or distance < 0 or distance >= len(tokenized_list):
+						context_row.append(0)
+						continue
+					context_row.append(word_set[tokenized_list[distance]])
+				context.append(context_row)
+				target.append(word_set[context_word])
+				if len(context) == batch_size or sent_indx == total_sentences - 1:
+					yield np.array(context), np.array(target)[..., np.newaxis]
 
 	return generate_pairs, len(word_set), word_set
 
 def initialize_parameters(vocab_size, embedding_size):
+	vocab_size += 1 # Adjust for padding
 	embeddings = tf.Variable(
 			tf.random_uniform(
 				[vocab_size, embedding_size],
@@ -112,43 +129,42 @@ def initialize_parameters(vocab_size, embedding_size):
 
 	return (embeddings, nce_mat, nce_bias)
 
-def model(vocab_size, embedding_size, pair, lr):
+def model(vocab_size, embedding_size, pair, lr, word_distance):
 	embeddings, nce_mat, nce_bias = initialize_parameters(vocab_size, embedding_size)
 	(inputs, labels) = pair	
 
 	embed = tf.nn.embedding_lookup(embeddings, inputs)
-	
+	sum_embed = tf.squeeze(tf.reduce_sum(embed, axis=1, keep_dims=True))
 	loss = tf.reduce_mean(
 			tf.nn.nce_loss(
 				weights = nce_mat,
 				biases = nce_bias,
 				labels = labels,
-				inputs = embed,
+				inputs = sum_embed,
 				num_sampled = 10,
 				num_classes = vocab_size
 			)
 		)
 	optimizer = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
 
-	output = embed @ tf.transpose(nce_mat) + nce_bias
+	output = sum_embed @ tf.transpose(nce_mat) + nce_bias
 
 	correct_prediction = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(output,1), tf.argmax(labels, 1)), tf.float32),name="accuracy")
 	return optimizer, loss, correct_prediction
 
 
-def train(embedding_size = 100, word_distance = 10, batch_size = 64, lr = 1e-3, epochs = 10):
+def train(embedding_size = 100, word_distance = 5, batch_size = 64, lr = 1e-3, epochs = 10):
 	tf.reset_default_graph()
 	timestamp = str(int(time.time()))
 	logdir = "model/"+timestamp+"/"
 
-	inputs = tf.placeholder(tf.int32, [None])
+	inputs = tf.placeholder(tf.int32, [None, 2*word_distance])
 	labels = tf.placeholder(tf.int32, [None, 1])
 	
-	document, train_x = imdb_dataset()
-	generate_pairs, vocab_size, word_set = preprocess(itr=document,tokenized=True)
+	#document, train_x = imdb_dataset()
+	generate_pairs, vocab_size, word_set = preprocess()
 	
-	
-	optimizer, loss, correct_prediction = model(vocab_size, embedding_size, (inputs, labels), lr)
+	optimizer, loss, correct_prediction = model(vocab_size, embedding_size, (inputs, labels), lr, word_distance)
 	
 	init = tf.global_variables_initializer()
 	with tf.Session() as sess:
@@ -158,7 +174,7 @@ def train(embedding_size = 100, word_distance = 10, batch_size = 64, lr = 1e-3, 
 			for e in range(epochs):	
 				epoch_loss = 0
 				epoch_acc = 0
-				for indx,pair in enumerate(generate_pairs(word_distance, batch_size, itr=document)):
+				for indx,pair in enumerate(generate_pairs(word_distance, batch_size)):
 					context, target = pair
 					_, c, a  = sess.run(
 							[optimizer, loss, correct_prediction],
@@ -169,16 +185,21 @@ def train(embedding_size = 100, word_distance = 10, batch_size = 64, lr = 1e-3, 
 					if indx % (500) == 0:
 						logger.info(f"epoch:{e} indx:{indx}, loss:{epoch_loss} ") 
 						print(f"epoch:{e} indx:{indx}, loss:{epoch_loss} ")
+					if memory_above(95):
+						log_str = f"epoch:{e} indx:{indx}, terminating! high memory usage"
+						logger.info(log_str) 				
+						print(log_str)
+						break
 				print(f"Accuracy {epoch_acc/indx}")
 			saver.save(sess, os.path.join(logdir, "model.ckpt"))
 		except KeyboardInterrupt as e:
 			print(str(e))
 			saver.save(sess, os.path.join(logdir, "model.ckpt"))
+		finally:
 			save_dictionary = input("Save the dictionary?")
 			if save_dictionary.lower() != "no":
 				with open(logdir+"words/word_set.pickle","wb") as f:
 					pickle.dump(word_set, f)
-
 def cosine_distance(v1, v2):
 	"""returns the cosine_distance"""
 	return np.dot(v1, v2)/(np.linalg.norm(v1)*np.linalg.norm(v2))
@@ -218,7 +239,8 @@ def test(path):
 
 
 if __name__ == "__main__":
-#	train(word_distance=5, batch_size=128, lr=1e-4)
+	train(word_distance = 2, batch_size=8, lr=1e-4)
+	print("Testing")
 	most_similar = test("model/1536953904/")
 	while True:
 		word = input(":")
